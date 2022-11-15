bcb835137dc69f9b1b184505367d67bb285e40f5
Puchnin78 <112937192+Puchnin78@users.noreply.github.com>
2022-11-15 22:35:12 +0700

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+void userInput();
+int max(int FirstNumber, int SecondNumber);
+void outputAnswer(int MaxNumber);
+int main() {
+    userInput();
+    return 0;
+}
+void userInput() {
+    int FirstNumber;
+    int SecondNumber;
+    std::cin >> FirstNumber >> SecondNumber;
+    outputAnswer(max(FirstNumber, FecondNumber));
+}
+int max(int firstNumber, int secondNumber) {
+    if (FirstNumber >= SecondNumber) return FirstNumber;
+    else return SecondNumber;
+}
+void outputAnswer(int MaxNumber) {
+    std::cout << MaxNumber;
+}
